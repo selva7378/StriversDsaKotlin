49f2b5e906e8b06b6050b9687a4fe6dc97c557cc
Selvaganesh M <selvaganesh7378@gmail.com>
2026-02-02 08:40:42 +0530

    completed RemoveOutermostParanthesis.kt string problem

--- src/String/RemoveOutermostParanthesis.kt
+++ src/String/RemoveOutermostParanthesis.kt
@@ -4,6 +4,21 @@ fun main() {
 
 }
 
-fun removeOutermostParanthesis(n: Int) {
-
+fun removeOuterParentheses(s: String): String {
+    var level = 0
+    var res = ""
+    for(letter in s) {
+        if( letter == '(') {
+            if (level > 0) {
+                res += letter
+            }
+            level++
+        } else if(letter  == ')') {
+            level--
+            if(level > 0) {
+                res += letter
+            }
+        }
+    }
+    return res
 }
