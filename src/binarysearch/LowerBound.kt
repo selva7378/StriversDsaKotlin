06aee9a7c7fd2bb471f01ecaab478efba4404df2
Selvaganesh M <selvaganesh7378@gmail.com>
2026-02-08 23:59:53 +0530

    completed lower bound

---
+++ src/binarysearch/LowerBound.kt
@@ -0,0 +1,23 @@
+package binarysearch
+
+fun main() {
+    println(lowerBound(intArrayOf(1,2,2,3), 2))
+    println(lowerBound(intArrayOf(3,5,8,15,19), 9))
+}
+
+fun lowerBound(arr: IntArray, target: Int): Int {
+    var ans = arr.size // default value if not found
+    var low = 0
+    var high = arr.size - 1
+    while (low <= high) {
+        val mid = (low + high) / 2
+        when {
+            arr[mid] < target -> low = mid + 1
+            arr[mid] >= target -> {
+                ans = mid
+                high = mid - 1
+            }
+        }
+    }
+    return ans
+}
